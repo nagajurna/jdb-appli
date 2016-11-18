1d54367c344685743dbc70a176cc4a135b288f7e
Paul <ppruneta@gmail.com>
2016-11-18 11:43:52 +0100

    sortService injection

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,1 @@
-var app = angular.module("jdbApp", ['ngRoute','ngSanitize','app.leaflet','app.authService','app.mapService','app.sortService', 'app.places', 'app.games', 'app.main', 'app.users', 'app.comments']);
+var app = angular.module("jdbApp", ['ngRoute','ngSanitize','app.leaflet','app.authService','app.mapService', 'app.places', 'app.games', 'app.main', 'app.users', 'app.comments','app.sortService']);
