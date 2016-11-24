3f6c5c18150bacb4b48e8a3abdb789e0d5cd1b9b
Paul <ppruneta@gmail.com>
2016-11-24 23:37:46 +0100

    force ssl

--- app.js
+++ app.js
@@ -18,6 +18,20 @@ var users = require('./routes/users')
 //**************
 
 var app = express();
+var env = process.env.NODE_ENV || 'development';
+var forceSsl = function(req, res, next) {
+	if (req.headers['x-forwarded-proto'] !== 'https') {
+		return res.redirect(['https://', req.get('Host'), req.url].join(''));
+	}
+		
+	return next();
+}
+
+if (env === 'production') {
+	app.use(forceSsl);
+}
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, '/public/views'));
